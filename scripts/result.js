ab400c69a830f873bf5ed1b0e47e0b685ba7f31b
black3snake <black3snake@mail.ru>
2025-03-17 16:58:06 +0800

    add correct.html only verstka

--- scripts/result.js
+++ scripts/result.js
@@ -2,10 +2,16 @@
     const Result = {
         init() {
             const url = new URL(location.href);
+            const correctAnswerElement = document.getElementById('correct');
             document.getElementById('result_score').innerText = url.searchParams.get('score') + '/' +
                 url.searchParams.get('total');
 
+            correctAnswerElement.onclick = function () {
+                location.href = 'correct.html';
+            }
+
         }
+
     }
 
 
